041c81aa903fe5ee3f9ce63600cd99b0de8f9d79
Kyle Lo <kyleclo@uw.edu>
2016-05-09 19:55:51 -0700

    Update insertion.cpp
    cleaned up code (removed namespace)

--- sorting/insertion/insertion.cpp
+++ sorting/insertion/insertion.cpp
@@ -1,51 +1,47 @@
-#include <iostream>
-#include <vector>
+# include <iostream>
+# include <vector>
 
-using namespace std;
+//using namespace std;
 
-void InsertionSort(vector <int> & x);
-void PrintArray(vector <int> & x);
-
+void insertionSort(std::vector<int>& x);
+void printVector(std::vector<int>& x);
 
 int main(){
 
-        int n = 6;
-        int values[] = {2, 5, 3, 6, 1, 4};
-
-        vector <int> x(&values[0], &values[n]);
-
-        InsertionSort(x);
-        PrintArray(x);
-
-        return 0;
+    std::vector<int> x = {2, 5, 3, 6, 1, 4};
+    
+    insertionSort(x);
+    printVector(x);
+    
+    return 0;
 }
 
-
-void InsertionSort(vector <int> & x){
-
-        int n = x.size();
-
-        for(int indexCandidate = 1; indexCandidate < n; indexCandidate++){
-
-                int candidate = x[indexCandidate];
-                int i = indexCandidate - 1;
-
-                while(i >= 0 && x[i] > candidate){
-                        x[i + 1] = x[i];
-                        i--;
-                }
-
-                x[i + 1] = candidate;
+void insertionSort(std::vector<int>& x){
+    
+    int n = x.size();
+    
+    for(int indexCandidate = 1; indexCandidate < n; indexCandidate++){
+        
+        int candidate = x[indexCandidate];
+        int indexInsertLocation = indexCandidate - 1;
+        
+        while(indexInsertLocation >= 0 && x[indexInsertLocation] > candidate){
+            
+            x[indexInsertLocation + 1] = x[indexInsertLocation];
+            indexInsertLocation--;
         }
+        
+        x[indexInsertLocation + 1] = candidate;
+    }
 }
 
 
-void PrintArray(vector <int> & x){
-
-        int n = x.size();
-
-        for(int i = 0; i < n; i++){
-                cout << x[i] << " ";
-        }
+void printVector(std::vector<int>& x){
+    
+    int n = x.size();
+    
+    for(int i = 0; i < n; i++)
+        std::cout << x[i] << " ";
+        
+    std::cout << std::endl;
 }
-
