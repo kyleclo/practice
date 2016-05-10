1d1108281b0f04666d8fdc232ac31071c0658a94
Kyle Lo <kyleclo@uw.edu>
2016-05-10 13:37:34 -0700

    Update merge.cpp
    modified to use std::vector

--- sorting/merge/merge.cpp
+++ sorting/merge/merge.cpp
@@ -1,84 +1,87 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
-
-void MergeSort(int x[], int indexStart, int indexEnd);
-void Merge(int x[], int indexStart, int indexMiddle, int indexEnd);
-void PrintArray(int x[], int n);
-
-
+void mergeSort(std::vector<int>& x, const int indexStart, const int indexEnd);
+void merge(std::vector<int>& x, const int indexStart, const int indexMiddle, const int indexEnd);
+void printVector(std::vector<int>& x);
 
 int main(){
-
-	int n = 6;
-	int x[] = {6,5,4,3,2,1};
-
-	MergeSort(x, 0, n - 1);
-	PrintArray(x, n);
-
-	return 0;
+    
+    std::vector<int> x = {2, 5, 3, 6, 1, 4};
+    
+    mergeSort(x, 0, x.size() - 1);
+    printVector(x);
+    
+    return 0;
 }
 
 
-void MergeSort(int x[], int indexStart, int indexEnd){
-
-	if(indexStart < indexEnd){     // if length = 1, no need to sort further
-
-		int indexMiddle = (indexEnd + indexStart) / 2;    // floors decimals by default
-		MergeSort(x, indexStart, indexMiddle);
-		MergeSort(x, indexMiddle + 1, indexEnd);
-		Merge(x, indexStart, indexMiddle, indexEnd);
-	}
-}
-
-void Merge(int x[], int indexStart, int indexMiddle, int indexEnd){
-
-	int n = indexEnd - indexStart + 1;
-	int* temp = new int[n];
-
-	int i = 0;
-	int indexLeft = indexStart;
-	int indexRight = indexMiddle + 1;
-
-	// while both left & right subarrays non-empty
-	while(indexLeft <= indexMiddle && indexRight <= indexEnd){
-
-		if(x[indexLeft] <= x[indexRight]){
-			temp[i] = x[indexLeft];
-			indexLeft++;
-		}  else{
-			temp[i] = x[indexRight];
-			indexRight++;
-		}
-
-		i++;
-	}
-
-	// if one subarray finishes early, continue with remaining
-	while(indexLeft <= indexMiddle){
-		temp[i] = x[indexLeft];
-		indexLeft++;
-		i++;
-	}
-
-	while(indexRight <= indexEnd){
-		temp[i] = x[indexRight];
-		indexRight++;
-		i++;
-	}
-
-	// replace x with sorted values in temp
-	for(int k = 0; k < n; k++){
-		x[indexStart + k] = temp[k];
-	}
-
-	delete[] temp;
+void mergeSort(std::vector<int>& x, const int indexStart, const int indexEnd){
+    
+    if(indexStart < indexEnd){      // if length = 1, no need to sort further
+        
+        int indexMiddle = indexStart + (indexEnd - indexStart) / 2;      // floors decimals by default
+        mergeSort(x, indexStart, indexMiddle);
+        mergeSort(x, indexMiddle + 1, indexEnd);
+        merge(x, indexStart, indexMiddle, indexEnd);
+    }
 }
 
 
-void PrintArray(int x[], int n){
+void merge(std::vector<int>& x, const int indexStart, const int indexMiddle, const int indexEnd){
+    
+    int n = indexEnd - indexStart + 1;
+    int* temp = new int[n];
+    
+    int indexTemp = 0;
+    int indexLeft = indexStart;
+    int indexRight = indexMiddle + 1;
+    
+    // while both left and right subarrays not-empty
+    while(indexLeft <= indexMiddle && indexRight <= indexEnd){
+        
+        if(x[indexLeft] <= x[indexRight]){
+            temp[indexTemp] = x[indexLeft];
+            indexLeft++;
+        }
+        else{
+            temp[indexTemp] = x[indexRight];
+            indexRight++;
+        }
+        
+        indexTemp++;
+    }
+    
+    
+    // if one subarray finishes early, continue with remaining
+    while(indexLeft <= indexMiddle){
+        temp[indexTemp] = x[indexLeft];
+        indexLeft++;
+        indexTemp++;
+    }
+    
+    while(indexRight <= indexEnd){
+        temp[indexTemp] = x[indexRight];
+        indexRight++;
+        indexTemp++;
+    }
+    
+    
+    // replace x with sorted values in temp
+    for(int i = 0; i < n; i++)
+        x[indexStart + i] = temp[i];
+    
+    
+    delete[] temp;
+}
 
-	for(int i = 0; i < n; i++){
-		cout << x[i] << " ";
-	}
+
+void printVector(std::vector<int>& x){
+    
+    int n = x.size();
+    
+    for(int i = 0; i < n; i++)
+        std::cout << x[i] << " ";
+    
+    std::cout << std::endl;
 }
