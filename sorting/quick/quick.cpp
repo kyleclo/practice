a1e79fc8e5d3361da118905043daa5622ed0312d
Kyle Lo <kyleclo@uw.edu>
2016-05-11 10:08:49 -0700

    Update quick.cpp
    switch to std::vector

--- sorting/quick/quick.cpp
+++ sorting/quick/quick.cpp
@@ -1,91 +1,95 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
-
-void QuickSortSlow(int x[], int indexStart, int indexEnd);
-void QuickSortFast(int x[], int indexStart, int indexEnd);
-void PrintArray(int x[], int n);
-
+void quickSortLomuto(std::vector<int>& x, const int indexStart, const int indexEnd);
+void quickSortHoare(std::vector<int>& x, const int indexStart, const int indexEnd);
+void printVector(std::vector<int>& x);
 
 
 int main(){
-
-	int n = 6;
-	int x[] = {2, 5, 3, 6, 3, 4};
-	QuickSortSlow(x, 0, n - 1);
-	PrintArray(x, n);
-
-	cout << endl;
-
-	int y[] = {2, 5, 3, 6, 3, 4};
-	QuickSortFast(y, 0, n - 1);
-
-	PrintArray(y, n);
-
-	return 0;
-}
-
-void QuickSortSlow(int x[], int indexStart, int indexEnd){
-
-	// length of x greater than 1
-	if(indexStart < indexEnd){
-
-		// partition
-		int pivot = x[indexEnd];
-		int indexMiddle = indexStart;
-
-		for(int i = indexStart; i < indexEnd; i++){
-
-			// if any x[i] < pivot, then swap it into left side
-			if(x[i] < pivot){
-				int temp = x[indexMiddle];
-				x[indexMiddle] = x[i];
-				x[i] = temp;
-				indexMiddle++;
-			}
-		}
-
-		// swap last value (pivot) into middle
-		x[indexEnd] = x[indexMiddle];
-		x[indexMiddle] = pivot;
-
-		// sort left/right subarrays
-		QuickSortSlow(x, indexStart, indexMiddle - 1);
-		QuickSortSlow(x, indexMiddle + 1, indexEnd);
-	}
+    
+    std::vector<int> x = {2, 5, 3, 6, 1, 4};
+    
+    quickSortLomuto(x, 0, x.size() - 1);
+    printVector(x);
+    
+    std::vector<int> y = {2, 5, 3, 6, 1, 4};
+    
+    quickSortHoare(y, 0, y.size() - 1);
+    printVector(y);
+    
+    return 0;
 }
 
 
-void QuickSortFast(int x[], int indexStart, int indexEnd){
-
-	if(indexStart < indexEnd){
-
-		int pivot = x[indexStart];
-		int indexLeft = indexStart;
-		int indexRight = indexEnd;
-
-		while(x[indexLeft] < pivot){ indexLeft++; }
-		while(x[indexRight] > pivot){ indexRight--; }
-
-		if(indexLeft <= indexRight){
-			int temp = x[indexLeft];
-			x[indexLeft] = x[indexRight];
-			x[indexRight] = temp;
-			indexLeft++;
-			indexRight--;
-		}
-
-		QuickSortFast(x, indexStart, indexRight);
-		QuickSortFast(x, indexLeft, indexEnd);
-	}
+void quickSortLomuto(std::vector<int>& x, const int indexStart, const int indexEnd){
+   
+   if(indexStart < indexEnd){       // if length = 1, no need to sort further
+       
+       int const pivot = x[indexEnd];       // see notes: pick pivot at Start or End
+       int indexMiddle = indexStart;        // see notes: indexMiddle at opposite end of pivot
+       
+       // skip pivot
+       for(int indexCandidate = indexStart; indexCandidate < indexEnd; indexCandidate++){
+           
+           // move everything less than pivot to the left subarray
+           if(x[indexCandidate] < pivot){
+               
+               int temp = x[indexMiddle];
+               x[indexMiddle] = x[indexCandidate];
+               x[indexCandidate] = temp;
+               
+               indexMiddle++;
+           }
+       }
+       
+       // swap pivot into middle
+       x[indexEnd] = x[indexMiddle];
+       x[indexMiddle] = pivot;
+       
+       // sort the subarrays
+       quickSortLomuto(x, indexStart, indexMiddle - 1);
+       quickSortLomuto(x, indexMiddle + 1, indexEnd);
+   }
 }
 
 
-
-void PrintArray(int x[], int n){
-
-	for(int i = 0; i < n; i++){
-		cout << x[i] << " ";
-	}
+void quickSortHoare(std::vector<int>& x, const int indexStart, const int indexEnd){
+    
+    if(indexStart < indexEnd){
+        
+        int const pivot = x[indexStart];
+        int indexLeft = indexStart;
+        int indexRight = indexEnd;
+        
+        while(x[indexLeft] < pivot)
+            indexLeft++;
+        
+        while(x[indexRight] > pivot)
+            indexRight--;
+        
+        if(indexLeft <= indexRight){
+            
+            int temp = x[indexLeft];
+            x[indexLeft] = x[indexRight];
+            x[indexRight] = temp;
+            
+            indexLeft++;
+            indexRight--;
+        }
+        
+        quickSortHoare(x, indexStart, indexRight);
+        quickSortHoare(x, indexLeft, indexEnd);
+    }
 }
 
+
+void printVector(std::vector<int>& x){
+    
+    int n = x.size();
+    
+    for(int i = 0; i < n; i++)
+        std::cout << x[i] << " ";
+    
+    std::cout << std::endl;
+}
