341ff3cf6669080f47bb336585b3721a736066d3
kyleclo <kyleclo@uw.edu>
2016-07-06 10:11:46 -0700

    python insertion sort

---
+++ sorting/insertion-sort.py
@@ -0,0 +1,31 @@
+"""
+
+Insertion sort
+
+"""
+
+import numpy as np
+
+
+def insertion_sort(x):
+    for index_candidate in range(1, len(x)):
+
+        candidate = x[index_candidate]
+        index_insert = index_candidate - 1
+
+        while index_insert >= 0 and x[index_insert] > candidate:
+            x[index_insert + 1] = x[index_insert]
+            index_insert += -1
+
+        x[index_insert + 1] = candidate
+
+
+def main():
+    x = np.random.permutation(10)
+    print 'Unsorted: ' + str(x)
+    insertion_sort(x)
+    print 'Sorted: ' + str(x)
+
+
+if __name__ == '__main__':
+    main()
